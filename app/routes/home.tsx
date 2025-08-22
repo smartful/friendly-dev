cc1b01903bfeaa0528f79c7d63d1f8e9a2c71e94
smartful <remi_rodrigues@hotmail.com>
2025-08-22 05:51:54 +0200

    initial commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,16 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "The Friendly Dev" },
+    { name: "description", content: "An Awesome Portfolio" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <h1>Friendly Dev</h1>
+    </>
+  );
 }
