837de9f481b9c3ff444db48acd4b299834bfeba6
smartful <remi_rodrigues@hotmail.com>
2025-09-05 03:36:29 +0200

    Fill Projects page

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,27 @@
-const ProjectsPage = () => {
+import type { Route } from './+types/index';
+import type { Project } from '~/types';
+import ProjectCard from '~/components/ProjectCard';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch('http://localhost:8000/projects');
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData as { projects: Project[] };
+  console.log(projects);
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
