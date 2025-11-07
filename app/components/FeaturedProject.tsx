523d5a6c2010cd75bda7957247f9fa8c2c303142
smartful <remi_rodrigues@hotmail.com>
2025-11-07 06:30:39 +0100

    Featured Project component

---
+++ app/components/FeaturedProject.tsx
@@ -0,0 +1,25 @@
+import type { Project } from '~/types';
+import ProjectCard from './ProjectCard';
+
+type FeaturedProjectProps = {
+  projects: Project[];
+  count: number;
+};
+
+const FeaturedProject = ({ projects, count = 4 }: FeaturedProjectProps) => {
+  const featured = projects.filter((project) => project.featured).slice(0, count);
+
+  return (
+    <section>
+      <h2 className="text-2xl font-bold mb-6 text-gray-200">⭐ Featured Projects</h2>
+
+      <div className="grid gap-6 sm:grid-cols-2">
+        {featured.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedProject;
--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,9 +1,28 @@
 import type { Route } from './+types/index';
+import FeaturedProject from '~/components/FeaturedProject';
+import type { Project } from '~/types';
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'The Friendly Dev | Welcome' }, { name: 'description', content: 'An Awesome Portfolio' }];
 }
 
-export default function Home() {
-  return <section>Home Page</section>;
+export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+
+  return { projects: data };
 }
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+
+  console.log('projects : ', projects);
+
+  return (
+    <>
+      <FeaturedProject projects={projects} count={2} />
+    </>
+  );
+};
+
+export default HomePage;
