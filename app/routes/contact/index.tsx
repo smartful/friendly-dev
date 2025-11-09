0213385f4538729af79221a74e4256d903c9ed8a
smartful <remi_rodrigues@hotmail.com>
2025-11-09 03:03:08 +0100

    manage data from form on Contact Page

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -1,8 +1,60 @@
-const ContactPage = () => {
+import type { Route } from './+types';
+import { Form } from 'react-router';
+
+export async function action({ request }: Route.ActionArgs) {
+  const formData = await request.formData();
+  const name = formData.get('name') as string;
+  const email = formData.get('email') as string;
+  const subject = formData.get('subject') as string;
+  const message = formData.get('message') as string;
+
+  const errors: Record<string, string> = {};
+
+  if (!name) {
+    errors.name = 'Name is required';
+  }
+
+  if (!email) {
+    errors.email = 'Email is required';
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    errors.email = 'Invalid email format';
+  }
+
+  if (!subject) {
+    errors.subject = 'Subject is required';
+  }
+
+  if (!message) {
+    errors.message = 'Message is required';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+
+  const data = {
+    name,
+    email,
+    subject,
+    message,
+  };
+
+  return { message: 'Form submitted successfully', data };
+}
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
+  const errors = actionData?.errors || {};
+
   return (
     <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className="text-3xl font-bold text-white mb-8 text-center">📫 Contact Me</h2>
-      <form className="space-y-6">
+
+      {actionData?.message ? (
+        <p className="mb-6 p-4 bg-green-700 text-green-100 text-center rounded-lg border border-green-500 shadow-md">
+          {actionData.message}
+        </p>
+      ) : null}
+      <Form method="post" className="space-y-6">
         <div>
           <label htmlFor="name" className="block text-sm font-medium text-gray-300">
             Full Name
@@ -13,6 +65,7 @@ const ContactPage = () => {
             name="name"
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
           />
+          {errors.name && <p className="text-red-400 text-sm mt-1">{errors.name}</p>}
         </div>
         <div>
           <label htmlFor="email" className="block text-sm font-medium text-gray-300">
@@ -24,6 +77,7 @@ const ContactPage = () => {
             name="email"
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
           />
+          {errors.email && <p className="text-red-400 text-sm mt-1">{errors.email}</p>}
         </div>
         <div>
           <label htmlFor="subject" className="block text-sm font-medium text-gray-300">
@@ -35,6 +89,7 @@ const ContactPage = () => {
             name="subject"
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
           />
+          {errors.subject && <p className="text-red-400 text-sm mt-1">{errors.subject}</p>}
         </div>
         <div>
           <label htmlFor="message" className="block text-sm font-medium text-gray-300">
@@ -46,11 +101,12 @@ const ContactPage = () => {
             rows={5}
             className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
           />
+          {errors.message && <p className="text-red-400 text-sm mt-1">{errors.message}</p>}
         </div>
         <button className="w-full bg-blue-600 text-white py-2 rounded-lg cursor-pointer hover:bg-blue-700">
           Envoyer le message
         </button>
-      </form>
+      </Form>
     </div>
   );
 };
