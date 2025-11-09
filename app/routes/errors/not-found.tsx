18a0b00c7b9aaa615ab4c347fc3583a00f8b56ff
smartful <remi_rodrigues@hotmail.com>
2025-11-09 03:28:00 +0100

    creation of NotFound page

---
+++ app/routes/errors/not-found.tsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router';
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center px-6 min-h-[70vh]">
+      <h1 className="text-6xl font-extrabold text-blue-400 mb-2">404</h1>
+      <h2 className="text-2xl font-semibold text-gray-100 mb-2">Page Not Found</h2>
+      <p className="text-gray-400 mb-6 max-w-md">Désolé, la page que vous recherché n'existe pas !</p>
+      <Link to="/" className="inline-block bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+        Go Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
