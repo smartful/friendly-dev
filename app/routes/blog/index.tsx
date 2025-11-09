2a6d11d9264a7488fd83073ec1a13be729684a80
smartful <remi_rodrigues@hotmail.com>
2025-11-09 07:26:13 +0100

    Blog page

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,30 @@
-const BlogPage = () => {
+import type { Route } from './+types';
+import type { PostMeta } from '~/types';
+import PostCard from '~/components/PostCard';
+
+export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  const data = await res.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-    </>
+      {posts.map((post) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
