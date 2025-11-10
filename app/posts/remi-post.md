4e03ba8bbed1f28bd32eef31b31ae8153bd9046c
smartful <remi_rodrigues@hotmail.com>
2025-11-10 09:54:19 +0100

    pagination on blog page

--- app/posts/remi-post.md
+++ app/posts/remi-post.md
@@ -1,3 +1,6 @@
 Ceci est mon post.
 
 ## Titre
+
+Bah voilà !
+Que veux tu que je raconte d'intéressant !!!
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import type { Route } from './+types';
 import type { PostMeta } from '~/types';
 import PostCard from '~/components/PostCard';
+import Pagination from '~/components/Pagination';
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL('/posts-meta.json', request.url);
@@ -20,14 +22,23 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 4;
   const { posts } = loaderData;
+
+  const totalPages = Math.ceil(posts.length / postsPerPage);
+  const indexOfLast = currentPage * postsPerPage;
+  const indexOfFirst = indexOfLast - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirst, indexOfLast);
 
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
-      {posts.map((post) => (
+      {currentPosts.map((post) => (
         <PostCard key={post.slug} post={post} />
       ))}
+
+      {totalPages > 1 && <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />}
     </div>
   );
 };
